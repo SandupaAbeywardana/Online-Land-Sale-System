295844df946f41c06302a1a43f8aa5b58c3e8bda
Darren-IT21800696 <110802175+Darren-IT21800696@users.noreply.github.com>
2022-11-05 20:32:00 +0530

    index js

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function colombo(){
+    window.location.replace("../html/colombo.html");
+}
+
+function gampaha(){
+    window.location.replace("../html/gampaha.html");
+}
+
+function kaluthara(){
+    window.location.replace("../html/kaluthara.html");
+}
+
+function kandy(){
+    window.location.replace("../html/kandy.html");
+}
+
+function galle(){
+    window.location.replace("../html/galle.html");
+}
+
+function matara(){
+    window.location.replace("../html/matara.html");
+}
+
+function nuwaraeliya(){
+    window.location.replace("../html/nuwaraeliya.html");
+}
