d40c8616cb2d40499b90250bdc45330a51447ba6
Sandupa Abeywardana <sandupa.isum@gmail.com>
2022-11-11 21:51:39 +0530

    changes gdk karaaa

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 function colombo(){
-    window.location.replace("../html/colombo.html");
+    window.location.replace("html/colombo.html");
 }
 
 function gampaha(){
-    window.location.replace("../html/gampaha.html");
+    window.location.replace("html/gampaha.html");
 }
 
-function kaluthara(){
-    window.location.replace("../html/kaluthara.html");
+function kalutara(){
+    window.location.replace("html/kalutara.html");
 }
 
 function kandy(){
-    window.location.replace("../html/kandy.html");
+    window.location.replace("html/kandy.html");
 }
 
 function galle(){
-    window.location.replace("../html/galle.html");
+    window.location.replace("html/galle.html");
 }
 
 function matara(){
-    window.location.replace("../html/matara.html");
+    window.location.replace("html/matara.html");
 }
 
 function nuwaraeliya(){
-    window.location.replace("../html/nuwaraeliya.html");
+    window.location.replace("html/nuwaraeliya.html");
 }
